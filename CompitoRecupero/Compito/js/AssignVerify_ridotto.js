9da2ba0585ad41acf7be3c97e26ca62bf35bc6f3
Lorenzo Asti <Lo@MBP-di-Lorenzo.homenet.telecomitalia.it>
2018-04-18 21:18:31 +0200

    Initial commit

--- CompitoRecupero/Compito/js/AssignVerify_ridotto.js
+++ CompitoRecupero/Compito/js/AssignVerify_ridotto.js
@@ -1,4 +1,4 @@
-
+xxfffghcghch
 
 function alertConsegna() {
 	conferma = confirm("Sei sicuro di voler consegnare il compito?")
@@ -12,7 +12,6 @@ function alertConsegna() {
 
 
 
-
 function assignAll(){
 	
 	/***********
@@ -21,44 +20,53 @@ function assignAll(){
 	randomAssignmentRangeK("num_es1.1",4,-6,6);
 	randomAssignmentRangeK("num_es1.2",4,-6,6);
 	randomAssignmentRangeK("num_es1.3",4,-6,6);
-	//randomAssignmentRangeK("num_es1.4",4,-6,6);
-
+	
 	/***********
 		ComNot
 	************/
 	randomAssignmentRangeKSciNot("a_es2.1","k_es2.1",4,-6,6);
 	randomAssignmentRangeKSciNot("a_es2.2","k_es2.2",4,-6,6);
 	randomAssignmentRangeKSciNot("a_es2.3","k_es2.3",4,-6,6);
-	//randomAssignmentRangeKSciNot("a_es2.4","k_es2.4",4,-6,6);
 
 	/***********
 		Approx
 	************/
-	randomAssignmentRangeK("n1_es3.1",4,-6,6);
-	randomAssignmentRangeK("n2_es3.1",4,-6,6);
-	randomAssignmentRangeK("d1_es3.1",4,-6,6);
-	randomAssignmentRangeK("d2_es3.1",4,-6,6);
-
-	/*
-	randomAssignmentRangeK("n1_es3.2",4,-6,6);
-	randomAssignmentRangeK("n2_es3.2",4,-6,6);
-	randomAssignmentRangeK("d1_es3.2",4,-6,6);
-	randomAssignmentRangeK("d2_es3.2",4,-6,6);
-	*/
+	//randomAssignmentRangeK("n1_es3.1",4,-6,6);
+	//randomAssignmentRangeK("n2_es3.1",4,-6,6);
+	//randomAssignmentRangeK("d1_es3.1",4,-6,6);
+	//randomAssignmentRangeK("d2_es3.1",4,-6,6);
 
 	/***************
 		Conv Easy
 	****************/
-	randomAssignmentRangeK("x_es4.1",3,-1,0); // kg -> g
-	//randomAssignmentRangeK("x_es4.2",3,-1,0); // hg -> mg
-	//randomAssignmentRangeK("x_es4.3",3,3,4); // cm -> km
 	randomAssignmentRangeK("x_es4.4",3,-1,0); // hm -> m
 	randomAssignmentRangeK("x_es4.5",3,-1,0); // hm2 -> m2
-	//randomAssignmentRangeK("x_es4.6",3,2,3); // dm2 -> dam2
 	randomAssignmentRangeK("x_es4.7",3,5,6); // cm3 -> m3
-	randomAssignmentRangeK("x_es4.8",3,0,1); // dm3 -> mm3	
 	randomAssignmentRangeK("x_es4.9",3,2,3); // kg/m3 -> g/cm3
-	//my_copy("x_es4.9","x_es4.10") // kg/m3 -> kg/dm3
+
+	/********************
+		Densità V S.I.
+	*********************/
+	randomAssignmenMaterialDensity("material_es5_1","material_es5_2","d_es5");	
+	randomAssignmentIntRangeK("m_es5",2,0,0,1,1);
+
+	/********************
+		Densità m unità
+	*********************/
+	randomAssignmenMaterialDensity("material_es6_1","material_es6_2","d_es6");	
+	randomAssignmentIntRangeK("V_es6",2,0,0,1,1);
+
+	/********************
+		Formule inverse
+	*********************/
+	randomAssignmenFrac4Let("NumSx_1","DenSx_1","NumDx_1","DenDx_1","NumSx_2","DenSx_2","NumDx_2","DenDx_2");
+
+	/********************
+		Densità r_cilindro
+	*********************/
+	randomAssignmenMaterialDensity("material_es8_1","material_es8_2","d_es8");	
+	randomAssignmentIntRangeK("m_es8",2,0,1,1,1);	
+	randomAssignmentIntRangeK("r_es8",2,0,0,1,1);
 }
 
 
@@ -70,132 +78,97 @@ function verifyAll(){
 	
 	var points_tot, points_avail, control;
 	
-	points_tot = 0;
-	points_avail = 0;
+	pointsTotRealized = 0;
+	pointsTotAvail = 0;
 	
 	/***********
 		SciNot
 	************/
-
 	result = verifySciNot("num_es1.1","a_es1.1","k_es1.1","result_es1.1","weight_es1.1");
-	points_tot += result[0];
-	points_avail += result[1];
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
 
 	result = verifySciNot("num_es1.2","a_es1.2","k_es1.2","result_es1.2","weight_es1.2");
-	points_tot += result[0];
-	points_avail += result[1];
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
 
 	result = verifySciNot("num_es1.3","a_es1.3","k_es1.3","result_es1.3","weight_es1.3");
-	points_tot += result[0];
-	points_avail += result[1];
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
 
-	/*
-	result = verifySciNot("num_es1.4","a_es1.4","k_es1.4","result_es1.4","weight_es1.4");
-	points_tot += result[0];
-	points_avail += result[1];
-	*/
-	
+		
 	/***********
 		ComNot
-	************/	
-
+	************/
 	result = verifyComNot("a_es2.1","k_es2.1","x_es2.1","result_es2.1","weight_es2.1");
-	points_tot += result[0];
-	points_avail += result[1];
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
 
 	result = verifyComNot("a_es2.2","k_es2.2","x_es2.2","result_es2.2","weight_es2.2");
-	points_tot += result[0];
-	points_avail += result[1];
-	
-	result = verifyComNot("a_es2.3","k_es2.3","x_es2.3","result_es2.3","weight_es2.3");
-	points_tot += result[0];
-	points_avail += result[1];
-	
-	/*
-	result = verifyComNot("a_es2.4","k_es2.4","x_es2.4","result_es2.4","weight_es2.4");
-	points_tot += result[0];
-	points_avail += result[1];
-	*/
-	
-	/***********
-		Approx
-	************/
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
 
-	result = verifyApprox("n1_es3.1","n2_es3.1","d1_es3.1","d2_es3.1","a_es3.1","k_es3.1","o_es3.1","result_es3.1","weight_es3.1");
-	points_tot += result[0];
-	points_avail += result[1];
+	result = verifyComNot("a_es2.3","k_es2.3","x_es2.3","result_es2.3","weight_es2.3");
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
 	
-	/*
-	result = verifyApprox("n1_es3.2","n2_es3.2","d1_es3.2","d2_es3.2","a_es3.2","k_es3.2","o_es3.2","result_es3.2","weight_es3.2");
-	points_tot += result[0];
-	points_avail += result[1];
-	*/
+	
+	
+	
 
 	/***************
 		Conv Easy
 	****************/
-
-	result = verifyConv("unit_x_es4.1","x_es4.1","unit_y_es4.1","y_es4.1","exp_es4.1","result_es4.1","weight_es4.1");
-	points_tot += result[0];
-	points_avail += result[1];
-
-	/*
-	result = verifyConv("unit_x_es4.2","x_es4.2","unit_y_es4.2","y_es4.2","exp_es4.2","result_es4.2","weight_es4.2");
-	points_tot += result[0];
-	points_avail += result[1];
-	*/
-
-	/*
-	result = verifyConv("unit_x_es4.3","x_es4.3","unit_y_es4.3","y_es4.3","exp_es4.3","result_es4.3","weight_es4.3");
-	points_tot += result[0];
-	points_avail += result[1];
-	*/
-
 	result = verifyConv("unit_x_es4.4","x_es4.4","unit_y_es4.4","y_es4.4","exp_es4.4","result_es4.4","weight_es4.4");
-	points_tot += result[0];
-	points_avail += result[1];
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
 
 	result = verifyConv("unit_x_es4.5","x_es4.5","unit_y_es4.5","y_es4.5","exp_es4.5","result_es4.5","weight_es4.5");
-	points_tot += result[0];
-	points_avail += result[1];
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
 	
-	/*
-	result = verifyConv("unit_x_es4.6","x_es4.6","unit_y_es4.6","y_es4.6","exp_es4.6","result_es4.6","weight_es4.6");
-	points_tot += result[0];
-	points_avail += result[1];
-	*/
-
 	result = verifyConv("unit_x_es4.7","x_es4.7","unit_y_es4.7","y_es4.7","exp_es4.7","result_es4.7","weight_es4.7");
-	points_tot += result[0];
-	points_avail += result[1];
-
-	result = verifyConv("unit_x_es4.8","x_es4.8","unit_y_es4.8","y_es4.8","exp_es4.8","result_es4.8","weight_es4.8");
-	points_tot += result[0];
-	points_avail += result[1];
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
 
 	/*******************
 		Conv Density
 	********************/
-
 	result = verifyConvDensity("x_es4.9","y_es4.9","result_es4.9","weight_es4.9")
-	points_tot += result[0];
-	points_avail += result[1];
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
 
-	/*
-	result = verifyConvDensity("x_es4.10","y_es4.10","result_es4.10","weight_es4.10");
-	points_tot += result[0];
-	points_avail += result[1];
-	*/
+	/**********************
+		Densità V S.I.
+	***********************/
+	result = verifyDensityVolumeSI("d_es5","m_es5","ansLet_es5","ansLetNum_es5","ansLetDen_es5","ansNum_es5","ansA_es5","ansK_es5","result_es5","pointsAvail_es5","points_es5");
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
 
-	document.getElementById("points_tot").innerHTML = points_tot;
-	document.getElementById("points_avail").innerHTML = points_avail;
+	/**********************
+		Densità m unità
+	***********************/
+	result = verifyDensityMassUnits("d_es6","V_es6","ansLet_es6","ansLetNum_es6","ansLetDen_es6","ansNumKg_es6","ansA_es6","ansK_es6","ansNumG_es6","result_es6","pointsAvail_es6","points_es6");
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
+
+	/**********************
+		Formule inverse
+	***********************/
+	result = verifyInvFormula4("NumSx_1","DenSx_1","NumDx_1","DenDx_1","result_0_Num","result_0_Den","result_1_Num","result_1_Den","result_2_Num","result_2_Den","result_3_Num","result_3_Den","result_es7","pointsAvail_es7","points_es7")	
+	pointsTotRealized += result[0];
+	pointsTotAvail += result[1];
 	
-	/***********************
-		Increment Control
-	************************/
 
-	control_text = document.getElementById("control").innerHTML;
-	control = parseInt(control_text, 10);
-	control += 1;
-	document.getElementById("control").innerHTML = control;
+	
+	
+	console.log(pointsTotRealized)
+	console.log(pointsTotAvail)
+
+	/*******************************
+		Write & Cripto Punteggio
+	********************************/
+	writeAndCriptoResult(pointsTotRealized,pointsTotAvail,"pointsTotRealized","pointsTotAvail","criptoRand","cripto");
+
+
 }
